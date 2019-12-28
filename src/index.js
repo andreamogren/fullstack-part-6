b937dde8d5cda8fee0feba56715e6415bf93e822
Andrea Mogren <mogrenandrea@gmail.com>
2019-12-28 16:37:42 +0100

    started with 6.3

--- src/index.js
+++ src/index.js
@@ -1,48 +1,17 @@
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
-import reducer from './reducer'
+import App from './App'
+import reducer from './reducers/anecdoteReducer'
 
 const store = createStore(reducer)
 
-const App = () => {
-  const good = () => {
-    store.dispatch({
-      type: 'GOOD'
-    })
-  }
-  const ok = () => {
-    store.dispatch({
-      type: 'OK'
-    })
-  }
-  const bad = () => {
-    store.dispatch({
-      type: 'BAD'
-    })
-  }
-  const zero = () => {
-    store.dispatch({
-      type: 'ZERO'
-    })
-  }
-
-  return (
-    <div>
-      <button onClick={good}>good</button>
-      <button onClick={ok}>neutral</button>
-      <button onClick={bad}>bad</button>
-      <button onClick={zero}>reset stats</button>
-      <div>good {store.getState().good}</div>
-      <div>neutral {store.getState().ok}</div>
-      <div>bad {store.getState().bad}</div>
-    </div>
+const render = () => {
+  ReactDOM.render(
+    <App store={store} />,
+    document.getElementById('root')
   )
 }
 
-const renderApp = () => {
-  ReactDOM.render(<App />, document.getElementById('root'))
-}
-
-renderApp()
-store.subscribe(renderApp)
+render()
+store.subscribe(render)
